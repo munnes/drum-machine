d30725fcd8c9c44d01e55077deaf46292e563a5c
Nasreen Sayed Siddig Elhussain <nasreen.store@gmail.com>
2019-10-19 21:17:17 +0400

    Updates

---
+++ precache-manifest.a928820d283f8de73655ff3a19ec6f26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23d58f2acfd93e7f3d9d5063d708c85",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "fd5954663851d09cef99",
+    "url": "/drum-machine/static/css/main.0aec8c29.chunk.css"
+  },
+  {
+    "revision": "045e03aadb4e49cc88ee",
+    "url": "/drum-machine/static/js/2.0a9a844a.chunk.js"
+  },
+  {
+    "revision": "fd5954663851d09cef99",
+    "url": "/drum-machine/static/js/main.0520b53d.chunk.js"
+  },
+  {
+    "revision": "f21ce7720cfc5fa2cd84",
+    "url": "/drum-machine/static/js/runtime-main.61d160df.js"
+  },
+  {
+    "revision": "07500d54fa2ddeb6c2ec37cdf1b16a5b",
+    "url": "/drum-machine/static/media/A.07500d54.mp3"
+  },
+  {
+    "revision": "00828a78b23174ab83d302669de708c9",
+    "url": "/drum-machine/static/media/C.00828a78.mp3"
+  },
+  {
+    "revision": "cd35575b99666c650d4a0a7e7fcc2b86",
+    "url": "/drum-machine/static/media/D.cd35575b.mp3"
+  },
+  {
+    "revision": "3cda00b7de0537b36d5236c3a404ae32",
+    "url": "/drum-machine/static/media/E.3cda00b7.mp3"
+  },
+  {
+    "revision": "f37ed6a083abf36a75a2ba61cb6a67f5",
+    "url": "/drum-machine/static/media/S.f37ed6a0.mp3"
+  },
+  {
+    "revision": "3d05968fc392a7ae4f7fcdfea653c13b",
+    "url": "/drum-machine/static/media/W.3d05968f.mp3"
+  },
+  {
+    "revision": "7bc5d64d4891e9a889ea742bb649d369",
+    "url": "/drum-machine/static/media/X.7bc5d64d.mp3"
+  },
+  {
+    "revision": "200de8e8cd514c9fa856d9735d473979",
+    "url": "/drum-machine/static/media/Z.200de8e8.mp3"
+  }
+]);
