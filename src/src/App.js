c611c7784304f738c1654bbc004260a7d83719a8
munnes <49619392+munnes@users.noreply.github.com>
2019-10-25 18:31:32 +0200

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import MyDrum from './MyDrum'
+class App extends Component {
+    render(){
+  return (
+    <div className="App">
+     <MyDrum />
+      </div>
+  );
+    }}
+
+export default App;
---
+++ src/src/playPad.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+const playPad = (id) => {
+    return{
+        type:'play-pad',
+        id
+    };
+}
+export  default playPad;
